c2afb6312d76cae0d23836d3ff8d382fbf9f8b26
trungnd2 <trungnd2@vng.com.vn>
2020-12-29 16:35:34 +0700

    faf

---
+++ src/main/java/vn/com/insee/corporate/controller/admin/AuthenAdminController.java
@@ -0,0 +1,11 @@
+package vn.com.insee.corporate.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/authen")
+public class AuthenAdminController {
+
+
+}
