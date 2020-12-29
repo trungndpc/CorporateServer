0af5fb49ca5bb6b4e72f2b33dd9c474ff241b7b0
trung <trungnd.pc@gmail.com>
2020-12-29 23:29:47 +0700

    update

--- src/main/java/vn/com/insee/corporate/config/BeanConfig.java
+++ src/main/java/vn/com/insee/corporate/config/BeanConfig.java
@@ -3,12 +3,24 @@ package vn.com.insee.corporate.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class BeanConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now());
+    }
+
 }
--- src/main/java/vn/com/insee/corporate/entity/BaseEntity.java
+++ src/main/java/vn/com/insee/corporate/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.sql.Timestamp;
@@ -14,9 +15,11 @@ import java.sql.Timestamp;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(name = "created_time")
     private Timestamp createdTime;
 
     @LastModifiedDate
+    @Column(name = "updated_time")
     private Timestamp updatedTime;
 
     public Timestamp getCreatedTime() {
