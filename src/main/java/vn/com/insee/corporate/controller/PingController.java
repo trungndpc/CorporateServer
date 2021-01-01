a299b4c0afa78f09b803675050578075240c8405
trung <trungnd.pc@gmail.com>
2021-01-02 00:31:40 +0700

    afa

--- src/main/java/vn/com/insee/corporate/controller/PingController.java
+++ src/main/java/vn/com/insee/corporate/controller/PingController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.com.insee.corporate.common.Permission;
 import vn.com.insee.corporate.entity.CustomerEntity;
 import vn.com.insee.corporate.entity.UserEntity;
 import vn.com.insee.corporate.repository.CustomerRepository;
@@ -25,18 +26,34 @@ public class PingController {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping("/ping")
+    @GetMapping("/delete-all")
     String all(Authentication authentication) {
-        List<CustomerEntity> all = customerRepository.findAll();
-        for (CustomerEntity e:
-             all) {
-            if (e.getId() != 1025) {
-                e.setLinkedUser(false);
-                customerRepository.save(e);
-            }
-        }
+        repository.deleteAll();
+        customerRepository.deleteAll();
         return "OK";
     }
+
+    @GetMapping("/add-admin")
+    String addAdmin(Authentication authentication) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName("Nguyễn Công Phượng");
+        userEntity.setAvatar("https://static2.yan.vn/YanNews/202006/202006030349344511-2fc01591-f2a8-45f8-a210-79f517c9297b.png");
+        userEntity.setPassword("1");
+        userEntity.setPassword("84972797200");
+        userEntity.setEnable(true);
+        userEntity.setStatus(1);
+        userEntity.setRoleId(Permission.ADMIN.getId());
+        repository.saveAndFlush(userEntity);
+        return "OK";
+    }
+
+    @GetMapping("/auto-gen")
+    String autoGen(Authentication authentication) {
+        genCustomer();
+        return "OK";
+    }
+
+
 
     protected void genCustomer() {
         List<CustomerEntity> all = customerRepository.findAll();
@@ -44,8 +61,8 @@ public class PingController {
             CustomerEntity customerEntity = all.get(i);
             for (int j = 10; j < 99; j++) {
                 CustomerEntity clone = new CustomerEntity();
-                clone.setFullName(customerEntity.getFullName() + " " + j);
-                clone.setPhone("09727971" + j);
+                clone.setFullName("Auto gen" + " " + j);
+                clone.setPhone("09227971" + j);
                 clone.setAvatar(customerEntity.getAvatar());
                 clone.setMainAreaId(1);
                 clone.setStatus(customerEntity.getStatus());
