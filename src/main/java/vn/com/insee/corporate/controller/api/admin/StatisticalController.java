55c0768b42f2f056eddcb6c39a4323cf53081c05
trungnd2 <trungnd.pc@gmail.com.vn>
2021-03-09 07:28:36 +0700

    dev ing...

---
+++ src/main/java/vn/com/insee/corporate/controller/api/admin/StatisticalController.java
@@ -0,0 +1,11 @@
+package vn.com.insee.corporate.controller.api.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/dashboard")
+public class DashboardController {
+
+
+}
